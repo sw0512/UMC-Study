ed91a181e6ec9773549ccc13674eda1ed72f434d
sw0512 <kimsu3047@gmail.com>
2026-03-16 10:07:48 +0900

    type 실습

---
+++ 1주차/type.ts
@@ -0,0 +1,34 @@
+// string
+const str1: string = 123;
+const str2: string = "umc";
+
+// number
+const num1: number = true;
+const num2: number = 123;
+
+// bollean
+const bol1: boolean = null;
+const bol2: boolean = true;
+
+//null
+const nul1: null = undefined;
+const nul2: null = null;
+
+//undefined
+const und1: undefined = Symbol('symbol');
+const und2: undefined = undefined;
+
+//symbol
+const sym1: symbol = BigInt(12345678901234567890);
+const sym2: symbol = Symbol('symbol');
+
+//bigint
+const big1: bigint = {yaho: 'yaho'};
+const big2: bigint = BigInt(12345678901234567890);
+
+//object
+const obj1: object = "umc";
+const obj2: object = {yaho: 'yaho'};
+
+
+
