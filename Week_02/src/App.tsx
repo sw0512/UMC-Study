03f3975b973b25161d2377ec1b8051a24768a91f
sw0512 <kimsu3047@gmail.com>
2026-03-20 16:37:44 +0900

    컴포넌트 분리 + 타입 지정

--- Week_02/src/App.tsx
+++ Week_02/src/App.tsx
@@ -1,10 +1,22 @@
 import './App.css'
+// 1) List 컴포넌트를 import해요.
+import List from './components/List';
 
 function App() {
+  const nickname = '매튜'
+  const sweetPotato = '고구마'
+  const array = ['REACT', 'NEXT', 'VUE', 'SVELTE', 'ANGULAR', 'REACT-NATIVE']
   return (
-    <>
-      <h1>Hello React</h1>
-    </>
+     <>
+      <strong className='school'>상명대학교</strong>
+      <p style={{color: 'purple', fontWeight:'bold', fontSize:'3rem'}}>{nickname}/김용민</p>
+      <h1>{`${nickname}는 ${sweetPotato} 아이스크림을 좋아합니다.`}</h1>
+      <ul>
+        {array.map((yaho, idx) => (
+          <List key={idx} tech={yaho} />
+        ))}
+      </ul>
+     </>
   )
 }
 
