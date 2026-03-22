2647458ce9e7ae1c5e3d44155f45a24604f4316c
sw0512 <kimsu3047@gmail.com>
2026-03-22 14:53:09 +0900

    useContext 실습

--- Week_02/src/App.tsx
+++ Week_02/src/App.tsx
@@ -1,23 +1,15 @@
-import './App.css'
-// 1) List 컴포넌트를 import해요.
-import List from './components/List';
+import ButtonGroup from './components/ButtonGroup';
+import { useCount } from './context/CounterProvider';
 
 function App() {
-  const nickname = '매튜'
-  const sweetPotato = '고구마'
-  const array = ['REACT', 'NEXT', 'VUE', 'SVELTE', 'ANGULAR', 'REACT-NATIVE']
+  const { count } = useCount();
+
   return (
-     <>
-      <strong className='school'>상명대학교</strong>
-      <p style={{color: 'purple', fontWeight:'bold', fontSize:'3rem'}}>{nickname}/김용민</p>
-      <h1>{`${nickname}는 ${sweetPotato} 아이스크림을 좋아합니다.`}</h1>
-      <ul>
-        {array.map((yaho, idx) => (
-          <List key={idx} tech={yaho} />
-        ))}
-      </ul>
-     </>
-  )
+    <>
+      <h1>{count}</h1>
+      <ButtonGroup />
+    </>
+  );
 }
 
-export default App
+export default App;
--- Week_02/src/main.tsx
+++ Week_02/src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+// main.tsx
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
+import { CounterProvider } from './context/CounterProvider.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <CounterProvider>
+      <App />
+    </CounterProvider>
+  </StrictMode>
+);
