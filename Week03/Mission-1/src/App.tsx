1e64f4d08396f7f381b29fa0f3756bf370ab8a1e
Snagwoo <kimsu3047@gmail.com>
2026-03-27 16:42:07 +0900

    feat: add movie fetching and display functionality with MovieCard component

--- Week03/Mission-1/src/App.tsx
+++ Week03/Mission-1/src/App.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
+// import React from 'react'
+import MoviePage from './pages/MoviePage';
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <MoviePage />
+    </>
   )
 }
 
---
+++ Week03/Mission-1/src/components/MovieCard.tsx
@@ -0,0 +1,37 @@
+// import React from 'react'
+import React from 'react';
+import type { Movie } from '../types/movie'
+
+interface MovieCardProps {
+  movie: Movie
+}
+
+const MovieCard = ({ movie }: MovieCardProps) => {
+
+  const [isHovered, setIsHovered] = React.useState(false);
+  console.log(isHovered);
+
+  return (
+    <div
+      className='relative rounded-xl shadow-lg overflow-hidden cursor-pointer w-48
+      transition-transform duration-500 hover:scale-105' 
+      onMouseEnter={() => setIsHovered(true)} 
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      <img src={`https://image.tmdb.org/t/p/w200${movie.poster_path}` }
+       alt={`${movie.title} 영화의 이미지`} 
+       className=''
+       />
+       {isHovered && (
+        <div className='absolute inset-0 text-white 
+          from-black/50 to-transparent backdrop-blur-md flex flex-col
+          justify-center items-center p-4'>
+          <h2 className='text-lg font-bold leading-snug'>{movie.title}</h2>
+          <p className='text-sm text-gray-300 leading-relaxed mt-2 line-clamp-5'>{movie.overview}</p>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default MovieCard
