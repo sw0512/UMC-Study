19b8a1d6c11a40f0f878453343fa4a0b869d23cb
Snagwoo <kimsu3047@gmail.com>
2026-04-18 18:35:21 +0900

    Mission-2

---
+++ Week05/Mission-2/src/layouts/HomeLayout.tsx
@@ -0,0 +1,33 @@
+import { Outlet, useNavigate } from 'react-router-dom'
+
+const HomeLayout = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div className="min-h-screen flex flex-col bg-[#0b0b12] text-white">
+      <nav className="border-b border-white/10 bg-black/30 px-6 py-4 backdrop-blur-sm">
+        내브바
+        <button onClick={() => navigate('/my')} className="absolute top-4 right-4 rounded-lg bg-[#807bff] px-4 py-2 text-sm font-semibold text-white shadow-lg shadow-[#807bff]/25 transition hover:bg-[#6b63d9] active:scale-[0.98]">
+        My Page
+        </button>
+        <button onClick={() => navigate('/login')} className="absolute top-4 right-30 rounded-lg bg-[#807bff] px-4 py-2 text-sm font-semibold text-white shadow-lg shadow-[#807bff]/25 transition hover:bg-[#6b63d9] active:scale-[0.98]">
+          Login
+        </button>
+        <button onClick={() => navigate('/signup')} className="absolute top-4 right-50 rounded-lg bg-[#807bff] px-4 py-2 text-sm font-semibold text-white shadow-lg shadow-[#807bff]/25 transition hover:bg-[#6b63d9] active:scale-[0.98]">
+          Signup
+        </button>
+        <button onClick={() => navigate('/')} className="absolute top-4 right-73 rounded-lg bg-[#807bff] px-4 py-2 text-sm font-semibold text-white shadow-lg shadow-[#807bff]/25 transition hover:bg-[#6b63d9] active:scale-[0.98]">
+          Home
+        </button>
+      </nav>
+      <main className='flex-1'>
+        <Outlet />
+      </main>
+      <footer className="border-t border-white/10 bg-black/30 px-6 py-4 text-sm text-white/60">
+        푸터
+      </footer>
+    </div>
+  )
+}
+
+export default HomeLayout
